036da1c2540ab5433be4390f0e5f4cbb273fc29b
Nathakorn <korn.za@windowslive.com>
2016-06-09 14:18:57 +0700

    Initial project

---
+++ EstValleyball/HomeController.swift
@@ -0,0 +1,35 @@
+//
+//  HomeController.swift
+//  EstValleyball
+//
+//  Created by KORN on 6/9/2559 BE.
+//  Copyright © 2559 Nathakorn Sukumsirichart. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
