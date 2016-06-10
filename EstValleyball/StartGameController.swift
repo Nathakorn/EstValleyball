63d4a1d5555cda8895af4b84e7815655486786f2
Nathakorn <korn.za@windowslive.com>
2016-06-10 11:54:54 +0700

    Add start game animation

--- EstValleyball/StartGameController.swift
+++ EstValleyball/StartGameController.swift
@@ -10,15 +10,39 @@ import UIKit
 
 class StartGameController: UIViewController {
 
+    @IBOutlet weak var keepBall: UIImageView!
+    var isImageBottom = true
     @IBAction func startGame(sender: UIButton) {
         self.performSegueWithIdentifier(SEGUE_STARTED_GAME, sender: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+       
+    }
+    override func viewDidAppear(animated: Bool) {
+        //var customFrame = keepBall.frame
+        
+        UIView.animateWithDuration(1.0, delay: 0.0, options: [UIViewAnimationOptions.Autoreverse, UIViewAnimationOptions.Repeat], animations: {
+                var originFrame = self.keepBall.frame
+                
+                originFrame.origin.y = 320.0
+                
+                self.keepBall.frame = originFrame
+            }, completion: { finished in
+        })
+        /*
+        UIView.animateWithDuration(1.0,
+            animations: {
+                
+            }, completion: { finished in
+                
+        })
+        */
+        //keepBall.frame = customFrame
+        //keepBall.image = UIImage(named: "07" )
+        //isImageLeftSide = !isImageLeftSide
         // Do any additional setup after loading the view.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
