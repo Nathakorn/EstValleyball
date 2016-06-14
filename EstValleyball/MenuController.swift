1ad2f4a83cc5e04fa9c79a77a76da8246a6ff6fd
warinporn khantithamaporn <Amour@warinporns-MacBook-Pro-3.local>
2016-06-15 01:09:13 +0700

    update menu

--- EstValleyball/MenuController.swift
+++ EstValleyball/MenuController.swift
@@ -12,37 +12,39 @@ class MenuController: UIViewController {
     var fromSegue: String!
     
     
-    @IBOutlet weak var playButton: UIButton!
-    @IBOutlet weak var ruleButton: UIButton!
-    @IBOutlet weak var youtubeButton: UIButton!
-    @IBOutlet weak var winnerButton: UIButton!
-    @IBOutlet weak var shareButton: UIButton!
-    @IBOutlet weak var howToPlayButton: UIButton!
+    var playButton = UIButton()
+    var ruleButton = UIButton()
+    var youtubeButton = UIButton()
+    var winnerButton = UIButton()
+    var shareButton = UIButton()
+    var howToPlayButton = UIButton()
     
+    var closeButton = UIButton()
    
-    @IBAction func playGame(sender: UIButton) {
-        playButton.setImage(UIImage(named: "btn_menu1_active.png"), forState: .Normal)
+    func playGame(sender: UIButton) {
+        self.playButton.setImage(UIImage(named: "btn_menu1_active.png"), forState: .Normal)
         self.performSegueWithIdentifier(SEGUE_SHOW_GAME, sender: nil)
     }
-    @IBAction func goRule(sender: UIButton) {
-        ruleButton.setImage(UIImage(named: "btn_menu2_active.png"), forState: .Normal)
+    func goRule(sender: UIButton) {
+        self.ruleButton.setImage(UIImage(named: "btn_menu2_active.png"), forState: .Normal)
     }
-    @IBAction func goHowToPlay(sender: UIButton) {
-        howToPlayButton.setImage(UIImage(named: "btn_menu3_active.png"), forState: .Normal)
+    func goHowToPlay(sender: UIButton) {
+        self.howToPlayButton.setImage(UIImage(named: "btn_menu3_active.png"), forState: .Normal)
     }
-    @IBAction func goYoutubeButton(sender: UIButton) {
-        youtubeButton.setImage(UIImage(named: "btn_menu4_active.png"), forState: .Normal)
+    func goYoutubeButton(sender: UIButton) {
+        self.youtubeButton.setImage(UIImage(named: "btn_menu4_active.png"), forState: .Normal)
     }
-    @IBAction func goWinner(sender: UIButton) {
-        winnerButton.setImage(UIImage(named: "btn_menu5_active.png"), forState: .Normal)
+    func goWinner(sender: UIButton) {
+        self.winnerButton.setImage(UIImage(named: "btn_menu5_active.png"), forState: .Normal)
     }
     
-    @IBAction func goShare(sender: UIButton) {
-         shareButton.setImage(UIImage(named: "btn_menu6_active.png"), forState: .Normal)
+    func goShare(sender: UIButton) {
+         self.shareButton.setImage(UIImage(named: "btn_menu6_active.png"), forState: .Normal)
     }
-    @IBAction func goStartGame(sender: UIButton) {
+    func goStartGame(sender: UIButton) {
         self.performSegueWithIdentifier(fromSegue, sender: nil)
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         if fromSegue == nil{
@@ -50,6 +52,42 @@ class MenuController: UIViewController {
         }
         //print(fromSegue)
         // Do any additional setup after loading the view.
+        
+        var screenSize = UIScreen.mainScreen().bounds.size
+        
+        var x: CGFloat = 34.0 / 320.0 * screenSize.width
+        var width: CGFloat = 252.0 / 320.0 * screenSize.width
+        var height: CGFloat = 23.0 / 568.0 * screenSize.width
+        
+        self.playButton.frame = CGRectMake(x, 82.0 / 568.0 * screenSize.height, width, height)
+        self.playButton.addTarget(self, action: #selector(MenuController.playGame), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.ruleButton.frame = CGRectMake(x, 134.0 / 568.0 * screenSize.height, width, height)
+        self.ruleButton.addTarget(self, action: #selector(MenuController.goRule), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.howToPlayButton.frame = CGRectMake(x, 188.0 / 568.0 * screenSize.height, width, height)
+        self.howToPlayButton.addTarget(self, action: #selector(MenuController.goHowToPlay), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.youtubeButton.frame = CGRectMake(x, 237.0 / 568.0 * screenSize.height, width, height)
+        self.youtubeButton.addTarget(self, action: #selector(MenuController.goYoutubeButton), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.winnerButton.frame = CGRectMake(x, 285.0 / 568.0 * screenSize.height, width, height)
+        self.winnerButton.addTarget(self, action: #selector(MenuController.goWinner), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.shareButton.frame = CGRectMake(x, 335.0 / 568.0 * screenSize.height, width, height)
+        self.shareButton.addTarget(self, action: #selector(MenuController.goShare), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.closeButton.frame = CGRectMake(138 / 320.0 * screenSize.width, 406.0 / 568.0 * screenSize.height, 44.0 / 320.0 * screenSize.width, 44.0 / 320.0 * screenSize.width)
+        self.closeButton.addTarget(self, action: #selector(MenuController.playGame), forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(self.playButton)
+        self.view.addSubview(self.ruleButton)
+        self.view.addSubview(self.howToPlayButton)
+        self.view.addSubview(self.youtubeButton)
+        self.view.addSubview(self.winnerButton)
+        self.view.addSubview(self.shareButton)
+        self.view.addSubview(self.closeButton)
+        
     }
 
     override func didReceiveMemoryWarning() {
